d601fe2cc272d1016f9a2af5ee1ea67232b4a7b7
JacobKeegan <jacobmkeegan@gmail.com>
2024-12-11 20:34:52 -0500

    Import cleanup.

--- src/test/java/gov/nysenate/openleg/api/legislation/law/LawCtrlBaseIT.java
+++ src/test/java/gov/nysenate/openleg/api/legislation/law/LawCtrlBaseIT.java
@@ -4,15 +4,12 @@ import gov.nysenate.openleg.api.ApiTest;
 import gov.nysenate.openleg.legislation.law.dao.LawFileDao;
 import gov.nysenate.openleg.processors.law.LawFile;
 import gov.nysenate.openleg.processors.law.ManagedLawProcessService;
-import gov.nysenate.openleg.search.law.ElasticLawSearchDao;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Collections;
 
 import static gov.nysenate.openleg.legislation.law.LawChapterCode.*;
-import static org.junit.Assert.fail;
 
 public class LawCtrlBaseIT extends ApiTest {
     @Autowired
